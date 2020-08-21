86384ba447ceb1dd70e0d3c7af713f421f06e712
adiyogi <adiyogisiddha@gmail.com>
2020-08-21 12:37:27 +0530

    Initial project setup for Jain baker recipe project

---
+++ src/main/java/com/jb/recipe/JainBakeryRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.jb.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JainBakeryRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JainBakeryRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jb/recipe/JainBakeryRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JainBakeryRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
